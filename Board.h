b4d79e45e7f1f177a902eb32f7c1143e899e5072
Rena Hofmeister <renaann@viper.cs.ksu.edu>
2018-11-18 14:18:31 -0600

    Updated Board.h and got core to compile

--- Board.h
+++ Board.h
@@ -1,25 +1,29 @@
 //Board struct
 
+#define x_size 11
+#define y_size 11
+
 typedef struct Board {
-    char board[11][11];
+    char board[x_size][y_size];
 } Board;
 
 Board * CreateBoard() {
-    Board * b = new * Board;
+    //Board * b = new * Board;
+    Board * b = malloc(x_size * sizeof(char));
     for(int i = 0; i < 11; i++) {
         b->board[0][i] = (char)i;
     }
     char c = 'A';
     for(int j = 1; j < 10; c++) {
         b->board[j][0] = c;
-        c++;
+        //c++;
     }
     for(int k = 1; k < 10; k++) {
         for(int l = 1; l < 10; l++) {
                 b->board[k][l] = '0';
         }
     }
-    return Board;
+    return b;
 }
 
 
